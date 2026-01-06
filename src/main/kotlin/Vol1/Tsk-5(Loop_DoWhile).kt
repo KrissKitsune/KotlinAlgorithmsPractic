ccf4f585dfc6468aec9653e7a45fc755ff88f978
KrissKitsune <krisskitsune@gmail.com>
2026-01-06 18:42:07 +0300

    Tsk about temperature in greenhouse. (Loop for)

--- src/main/kotlin/Vol1/Tsk-5(Loop_DoWhile).kt
+++ src/main/kotlin/Vol1/Tsk-5(Loop_DoWhile).kt
@@ -1,5 +1,8 @@
 package Vol1
 
+const val MIN_TEMPERATURE_IN_GREENHOUSE = 10
+const val MAX_TEMPERATURE_IN_GREENHOUSE = 35
+
 fun main() {
     val listOfTemperatureInGreenhouse = mutableListOf<Int>()
     var successMeasuredCount = 0
@@ -9,7 +12,8 @@ fun main() {
         val currentTemperatureInGreenhouse = readln().toInt()
         successMeasuredCount += 1
         listOfTemperatureInGreenhouse.add(currentTemperatureInGreenhouse)
-    } while ((currentTemperatureInGreenhouse >= 10) && (currentTemperatureInGreenhouse <= 36))
+    } while ((currentTemperatureInGreenhouse >= MIN_TEMPERATURE_IN_GREENHOUSE)
+        && (currentTemperatureInGreenhouse <= MAX_TEMPERATURE_IN_GREENHOUSE))
 
     println("Успешных попыток ${successMeasuredCount - 1}")
 
