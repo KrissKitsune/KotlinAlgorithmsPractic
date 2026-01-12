65c583a18f6d43c9ba77f695ec75f7e914138833
KrissKitsune <krisskitsune@gmail.com>
2026-01-12 10:16:48 +0300

    Tsk-2. Fix some newline characters

--- src/main/kotlin/Vol1/Tsk-2(Loop_While).kt
+++ src/main/kotlin/Vol1/Tsk-2(Loop_While).kt
@@ -2,10 +2,10 @@ package Vol1
 
 const val ONE_WATERING = 15
 const val SOIL_MOISTURE_MAX = 100
+
 fun main() {
     var soilMoistureNow = 30
     while (soilMoistureNow < SOIL_MOISTURE_MAX) {
-
         soilMoistureNow += ONE_WATERING
         println("Добавили $ONE_WATERING. Теперь влажность почвы равна $soilMoistureNow")
         Thread.sleep(3000)
